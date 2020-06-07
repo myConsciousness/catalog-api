0e636e9074747dbc970e80ca69aac578c7626ba4
myConsciousness <yourdream28@gmail.com>
2020-06-07 21:15:23 +0900

    first commit

---
+++ src/main/java/org/thinkit/common/catalog/Extension.java
@@ -0,0 +1,73 @@
+/**
+ * Project Name : dev-utils<br>
+ * File Name : Extension.java<br>
+ * Encoding : UTF-8<br>
+ * Creation Date : 2020/06/07<br>
+ * <p>
+ * Copyright © 2020 Kato Shinya. All rights reserved.
+ * <p>
+ * This source code or any portion thereof must not be<br>
+ * reproduced or used in any manner whatsoever.
+ */
+
+package org.thinkit.common.catalog;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * 拡張子を管理するカタログです。<br>
+ * {@link #getExtension()}を使用することで拡張子要素の文字列表現を取得することができます。<br>
+ * <br>
+ * 以下の静的メソッドを使用することでも各要素の文字列表現を取得できます。<br>
+ * {@link #java()}<br>
+ * {@link #xml()}<br>
+ * 
+ * @author Kato Shinya
+ * @since 1.0
+ * @version 1.0
+ */
+@Getter
+@RequiredArgsConstructor
+public enum Extension implements Catalog<Extension> {
+
+    /**
+     * Java
+     */
+    JAVA(0, ".java"),
+
+    /**
+     * XML
+     */
+    XML(1, ".xml");
+
+    /**
+     * コード値
+     */
+    private final int code;
+
+    /**
+     * 拡張子
+     */
+    private final String extension;
+
+    /**
+     * Javaの拡張子を返却します。
+     * 
+     * @return Javaの拡張子
+     * @see #JAVA
+     */
+    public static String java() {
+        return JAVA.getExtension();
+    }
+
+    /**
+     * XMLの拡張子を返却します。
+     * 
+     * @return XMLの拡張子
+     * @see #XML
+     */
+    public static String xml() {
+        return JAVA.getExtension();
+    }
+}
---
+++ src/main/java/org/thinkit/common/catalog/HtmlTag.java
@@ -0,0 +1,73 @@
+/**
+ * Project Name : dev-utils<br>
+ * File Name : HtmlTag.java<br>
+ * Encoding : UTF-8<br>
+ * Creation Date : 2020/06/07<br>
+ * <p>
+ * Copyright © 2020 Kato Shinya. All rights reserved.
+ * <p>
+ * This source code or any portion thereof must not be<br>
+ * reproduced or used in any manner whatsoever.
+ */
+
+package org.thinkit.common.catalog;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * HTMLタグを管理するカタログです。<br>
+ * {@link #getHtmlTag()}を使用することでHTMLタグ要素の文字列表現を取得することができます。<br>
+ * <br>
+ * 以下の静的メソッドを使用することでも各要素の文字列表現を取得できます。<br>
+ * {@link #br()}<br>
+ * {@link #p()}<br>
+ * 
+ * @author Kato Shinya
+ * @since 1.0
+ * @version 1.0
+ */
+@Getter
+@RequiredArgsConstructor
+public enum HtmlTag implements Catalog<HtmlTag> {
+
+    /**
+     * 改行
+     */
+    BREAK(0, "<br>"),
+
+    /**
+     * パラグラフ
+     */
+    PARAGRAPH(1, "<p>");
+
+    /**
+     * コード値
+     */
+    private final int code;
+
+    /**
+     * HTMLタグ
+     */
+    private final String htmlTag;
+
+    /**
+     * 改行のHTMLタグを返却します。
+     * 
+     * @return 改行のHTMLタグ
+     * @see #BREAK
+     */
+    public static String br() {
+        return BREAK.getHtmlTag();
+    }
+
+    /**
+     * パラグラフのHTMLタグを返却します。
+     * 
+     * @return パラグラフのHTMLタグ
+     * @see #PARAGRAPH
+     */
+    public static String p() {
+        return PARAGRAPH.getHtmlTag();
+    }
+}
