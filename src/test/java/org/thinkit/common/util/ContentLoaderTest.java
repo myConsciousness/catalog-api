326c8dfb1e9e007af9a57a0b422fc45814764f4f
myConsciousness <yourdream28@gmail.com>
2020-06-28 22:45:32 +0900

    update

--- src/test/java/org/thinkit/common/util/ContentLoaderTest.java
+++ src/test/java/org/thinkit/common/util/ContentLoaderTest.java
@@ -90,6 +90,29 @@ public final class ContentLoaderTest {
         }
     }
 
+    /**
+     * <pre>
+     * ❏ 概要
+     * {@link ContentLoader} クラスの {@link ContentLoader#load(String, List)} メソッドの返却値を確認する。
+     * このテストでは選択ノードの個数が小規模のコンテンツファイルを使用する。
+     * </pre>
+     * 
+     * <pre>
+     * ❏ 観点
+     * ・{@link ContentLoader#load(String, List)} から取得したリストが {@code null} ではないこと
+     * ・{@link ContentLoader#load(String, List)} から取得したリストが空リストではないこと
+     * ・キー名 <code>"test1"</code> に紐づく項目の値が <code>"0"</code> であること
+     * ・キー名 <code>"test2"</code> に紐づく項目の値が <code>"false"</code> であること
+     * ・キー名 <code>"test3"</code> に紐づく項目の値が <code>"0L"</code> であること
+     * ・キー名 <code>"test4"</code> に紐づく項目の値が <code>"1.0"</code> であること
+     * ・キー名 <code>"test5"</code> に紐づく項目の値が <code>"test"</code> であること
+     * </pre>
+     * 
+     * <pre>
+     * ❏ 留意点
+     * このテストケースおよび期待値は使用するテスト用のコンテンツに定義されたキーと値に依存しています。
+     * </pre>
+     */
     @Test
     public void testLoadWithSmallSelectionNodes() {
 
@@ -108,6 +131,29 @@ public final class ContentLoaderTest {
         }
     }
 
+    /**
+     * <pre>
+     * ❏ 概要
+     * {@link ContentLoader} クラスの {@link ContentLoader#load(String, List)} メソッドの返却値を確認する。
+     * このテストでは選択ノードの個数が中規模のコンテンツファイルを使用する。
+     * </pre>
+     * 
+     * <pre>
+     * ❏ 観点
+     * ・{@link ContentLoader#load(String, List)} から取得したリストが {@code null} ではないこと
+     * ・{@link ContentLoader#load(String, List)} から取得したリストが空リストではないこと
+     * ・キー名 <code>"test1"</code> に紐づく項目の値が <code>"0"</code> であること
+     * ・キー名 <code>"test2"</code> に紐づく項目の値が <code>"false"</code> であること
+     * ・キー名 <code>"test3"</code> に紐づく項目の値が <code>"0L"</code> であること
+     * ・キー名 <code>"test4"</code> に紐づく項目の値が <code>"1.0"</code> であること
+     * ・キー名 <code>"test5"</code> に紐づく項目の値が <code>"test"</code> であること
+     * </pre>
+     * 
+     * <pre>
+     * ❏ 留意点
+     * このテストケースおよび期待値は使用するテスト用のコンテンツに定義されたキーと値に依存しています。
+     * </pre>
+     */
     @Test
     public void testLoadWithMediumSelectionNodes() {
 
@@ -126,6 +172,29 @@ public final class ContentLoaderTest {
         }
     }
 
+    /**
+     * <pre>
+     * ❏ 概要
+     * {@link ContentLoader} クラスの {@link ContentLoader#load(String, List)} メソッドの返却値を確認する。
+     * このテストでは選択ノードの個数が大規模のコンテンツファイルを使用する。
+     * </pre>
+     * 
+     * <pre>
+     * ❏ 観点
+     * ・{@link ContentLoader#load(String, List)} から取得したリストが {@code null} ではないこと
+     * ・{@link ContentLoader#load(String, List)} から取得したリストが空リストではないこと
+     * ・キー名 <code>"test1"</code> に紐づく項目の値が <code>"0"</code> であること
+     * ・キー名 <code>"test2"</code> に紐づく項目の値が <code>"false"</code> であること
+     * ・キー名 <code>"test3"</code> に紐づく項目の値が <code>"0L"</code> であること
+     * ・キー名 <code>"test4"</code> に紐づく項目の値が <code>"1.0"</code> であること
+     * ・キー名 <code>"test5"</code> に紐づく項目の値が <code>"test"</code> であること
+     * </pre>
+     * 
+     * <pre>
+     * ❏ 留意点
+     * このテストケースおよび期待値は使用するテスト用のコンテンツに定義されたキーと値に依存しています。
+     * </pre>
+     */
     @Test
     public void testLoadWithLargeSelectionNodes() {
 
@@ -144,15 +213,23 @@ public final class ContentLoaderTest {
         }
     }
 
-    @Test
-    public void testLoadWhenAttributeListIsEmpty() {
-
-        final Throwable exception = assertThrows(IllegalArgumentException.class,
-                () -> ContentLoader.load("test", new ArrayList<>(0)));
-        assertNotNull(exception);
-        assertEquals("wrong parameter was given. Attribute is required.", exception.getMessage());
-    }
-
+    /**
+     * <pre>
+     * ❏ 概要
+     * {@link ContentLoader} クラスの {@link ContentLoader#load(String, List)} メソッドの引数として空のコンテンツ名が渡された際の機能を確認する。
+     * </pre>
+     * 
+     * <pre>
+     * ❏ 観点
+     * ・例外として {@link IllegalArgumentException} が発生すること
+     * ・例外発生時のメッセージが <code>"wrong parameter was given. Content name is required."</code> であること
+     * </pre>
+     * 
+     * <pre>
+     * ❏ 留意点
+     * なし
+     * </pre>
+     */
     @Test
     public void testLoadWhenContentNameIsEmpty() {
 
@@ -160,6 +237,32 @@ public final class ContentLoaderTest {
                 () -> ContentLoader.load("", TEST_ATTRIBUTE_LIST));
         assertNotNull(exception);
         assertEquals("wrong parameter was given. Content name is required.", exception.getMessage());
+    }
+
+    /**
+     * <pre>
+     * ❏ 概要
+     * {@link ContentLoader} クラスの {@link ContentLoader#load(String, List)} メソッドの引数として空のアトリビュートリストが渡された際の機能を確認する。
+     * </pre>
+     * 
+     * <pre>
+     * ❏ 観点
+     * ・例外として {@link IllegalArgumentException} が発生すること
+     * ・例外発生時のメッセージが <code>"wrong parameter was given. Attribute is required."</code> であること
+     * </pre>
+     * 
+     * <pre>
+     * ❏ 留意点
+     * なし
+     * </pre>
+     */
+    @Test
+    public void testLoadWhenAttributeListIsEmpty() {
+
+        final Throwable exception = assertThrows(IllegalArgumentException.class,
+                () -> ContentLoader.load("test", new ArrayList<>(0)));
+        assertNotNull(exception);
+        assertEquals("wrong parameter was given. Attribute is required.", exception.getMessage());
     }
 
     @Test
