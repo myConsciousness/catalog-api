aa6f52420029115b9c7a1856a67bcbb62b60ae22
Kato Shinya <yourdream28@gmail.com>
2020-07-09 15:41:22 +0900

    update

--- src/test/java/org/thinkit/common/util/FluentReflectionTest.java
+++ src/test/java/org/thinkit/common/util/FluentReflectionTest.java
@@ -1,3 +1,15 @@
+/**
+ * Project Name : dev-utils<br>
+ * File Name : FluentReflectionTest.java<br>
+ * Encoding : UTF-8<br>
+ * Creation Date : 2020/07/09<br>
+ * <p>
+ * Copyright © 2020 Kato Shinya. All rights reserved.
+ * <p>
+ * This source code or any portion thereof must not be<br>
+ * reproduced or used in any manner whatsoever.
+ */
+
 package org.thinkit.common.util;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -7,7 +19,14 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-public final class FluentReflectionTest {
+/**
+ * {@link FluentReflectionTest} クラスのテストクラスです。
+ * 
+ * @author Kato Shinya
+ * @since 1.0
+ * @version 1.0
+ */
+final class FluentReflectionTest {
 
     /**
      * {@link FluentReflection#invoke(String)} メソッドのテストメソッドを定義するテストクラスです。
@@ -30,6 +49,25 @@ public final class FluentReflectionTest {
          */
         private static final String STR_FAILURE = "failure";
 
+        /**
+         * <pre>
+         * ❏ 概要
+         * {@link FluentReflection} クラスの {@link FluentReflection#invoke(String)} メソッドの返却値を確認する。
+         * テストデータセットは {@link ReflectionTestDataSet#returnStringWithNoArgument()} を使用する。
+         * </pre>
+         * 
+         * <pre>
+         * ❏ 観点
+         * ・{@link FluentReflection#invoke(String)} メソッドから取得した値が <code>null</code> ではないこと
+         * ・{@link FluentReflection#invoke(String)} メソッドから取得した値が空文字列ではないこと
+         * ・{@link FluentReflection#invoke(String)} メソッドから取得した値が <code>"success"</code> であること
+         * </pre>
+         * 
+         * <pre>
+         * ❏ 留意点
+         * なし
+         * </pre>
+         */
         @Test
         void testReturnStringWithNoArgument() {
             final FluentReflection<String> reflection = new FluentReflection<>(ReflectionTestDataSet.class);
@@ -38,6 +76,64 @@ public final class FluentReflectionTest {
             assertNotNull(actualResult);
             assertTrue(!actualResult.isEmpty());
             assertEquals(STR_SUCCESS, actualResult);
+        }
+
+        /**
+         * <pre>
+         * ❏ 概要
+         * {@link FluentReflection} クラスの {@link FluentReflection#invoke(String)} メソッドの返却値を確認する。
+         * テストデータセットは {@link ReflectionTestDataSet#returnStringWithArgument(String)} を使用する。
+         * </pre>
+         * 
+         * <pre>
+         * ❏ 観点
+         * ・{@link FluentReflection#invoke(String)} メソッドから取得した値が <code>null</code> ではないこと
+         * ・{@link FluentReflection#invoke(String)} メソッドから取得した値が空文字列ではないこと
+         * ・{@link FluentReflection#invoke(String)} メソッドから取得した値が <code>"success"</code> であること
+         * </pre>
+         * 
+         * <pre>
+         * ❏ 留意点
+         * なし
+         * </pre>
+         */
+        @Test
+        void testReturnStringSuccessWithArgument() {
+            final FluentReflection<String> reflection = new FluentReflection<>(ReflectionTestDataSet.class);
+            final String actualResult = reflection.add(String.class, "test").invoke("returnStringWithArgument");
+
+            assertNotNull(actualResult);
+            assertTrue(!actualResult.isEmpty());
+            assertEquals(STR_SUCCESS, actualResult);
+        }
+
+        /**
+         * <pre>
+         * ❏ 概要
+         * {@link FluentReflection} クラスの {@link FluentReflection#invoke(String)} メソッドの返却値を確認する。
+         * テストデータセットは {@link ReflectionTestDataSet#returnStringWithArgument(String)} を使用する。
+         * </pre>
+         * 
+         * <pre>
+         * ❏ 観点
+         * ・{@link FluentReflection#invoke(String)} メソッドから取得した値が <code>null</code> ではないこと
+         * ・{@link FluentReflection#invoke(String)} メソッドから取得した値が空文字列ではないこと
+         * ・{@link FluentReflection#invoke(String)} メソッドから取得した値が <code>"failure"</code> であること
+         * </pre>
+         * 
+         * <pre>
+         * ❏ 留意点
+         * なし
+         * </pre>
+         */
+        @Test
+        void testReturnStringFailureWithArgument() {
+            final FluentReflection<String> reflection = new FluentReflection<>(ReflectionTestDataSet.class);
+            final String actualResult = reflection.add(String.class, "").invoke("returnStringWithArgument");
+
+            assertNotNull(actualResult);
+            assertTrue(!actualResult.isEmpty());
+            assertEquals(STR_FAILURE, actualResult);
         }
     }
 }
