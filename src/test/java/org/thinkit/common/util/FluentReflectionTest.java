4cd4163bac39d800609fcd8b16ded269438cc5f6
Kato Shinya <yourdream28@gmail.com>
2020-07-09 18:11:11 +0900

    update

--- src/test/java/org/thinkit/common/util/FluentReflectionTest.java
+++ src/test/java/org/thinkit/common/util/FluentReflectionTest.java
@@ -135,5 +135,136 @@ final class FluentReflectionTest {
             assertTrue(!actualResult.isEmpty());
             assertEquals(STR_FAILURE, actualResult);
         }
+
+        /**
+         * <pre>
+         * ❏ 概要
+         * {@link FluentReflection} クラスの {@link FluentReflection#invoke(String)} メソッドの返却値を確認する。
+         * テストデータセットは {@link ReflectionTestDataSet#returnStringWithArguments(String, int, boolean)} を使用する。
+         * </pre>
+         * 
+         * <pre>
+         * ❏ 観点
+         * ・{@link FluentReflection#invoke(String)} メソッドから取得した値が <code>null</code> ではないこと
+         * ・{@link FluentReflection#invoke(String)} メソッドから取得した値が空文字列ではないこと
+         * ・{@link FluentReflection#invoke(String)} メソッドから取得した値が <code>"success"</code> であること
+         * </pre>
+         * 
+         * <pre>
+         * ❏ 留意点
+         * なし
+         * </pre>
+         */
+        @Test
+        void testReturnStringWithArguments() {
+            final FluentReflection<String> reflection = new FluentReflection<>(ReflectionTestDataSet.class);
+            reflection.add(String.class, "").add(int.class, 0).add(boolean.class, true);
+            final String actualResult = reflection.invoke("returnStringWithArguments");
+
+            assertNotNull(actualResult);
+            assertTrue(!actualResult.isEmpty());
+            assertEquals(STR_SUCCESS, actualResult);
+        }
+
+        /**
+         * <pre>
+         * ❏ 概要
+         * {@link FluentReflection} クラスの {@link FluentReflection#invoke(String)} メソッドの返却値を確認する。
+         * テストデータセットは {@link ReflectionTestDataSet#returnIntegerWithNoArgument()} を使用する。
+         * </pre>
+         * 
+         * <pre>
+         * ❏ 観点
+         * ・{@link FluentReflection#invoke(String)} メソッドから取得した値が <code>1</code> であること
+         * </pre>
+         * 
+         * <pre>
+         * ❏ 留意点
+         * なし
+         * </pre>
+         */
+        @Test
+        void testReturnIntegerWithNoArgument() {
+            final FluentReflection<Integer> reflection = new FluentReflection<>(ReflectionTestDataSet.class);
+            final int actualResult = reflection.invoke("returnIntegerWithNoArgument");
+
+            assertEquals(1, actualResult);
+        }
+
+        /**
+         * <pre>
+         * ❏ 概要
+         * {@link FluentReflection} クラスの {@link FluentReflection#invoke(String)} メソッドの返却値を確認する。
+         * テストデータセットは {@link ReflectionTestDataSet#returnIntegerWithArgument(boolean)} を使用する。
+         * </pre>
+         * 
+         * <pre>
+         * ❏ 観点
+         * ・{@link FluentReflection#invoke(String)} メソッドから取得した値が <code>1</code> であること
+         * </pre>
+         * 
+         * <pre>
+         * ❏ 留意点
+         * なし
+         * </pre>
+         */
+        @Test
+        void testReturnIntegerTrueWithArgument() {
+            final FluentReflection<Integer> reflection = new FluentReflection<>(ReflectionTestDataSet.class);
+            final int actualResult = reflection.add(boolean.class, true).invoke("returnIntegerWithArgument");
+
+            assertEquals(1, actualResult);
+        }
+
+        /**
+         * <pre>
+         * ❏ 概要
+         * {@link FluentReflection} クラスの {@link FluentReflection#invoke(String)} メソッドの返却値を確認する。
+         * テストデータセットは {@link ReflectionTestDataSet#returnIntegerWithArgument(boolean)} を使用する。
+         * </pre>
+         * 
+         * <pre>
+         * ❏ 観点
+         * ・{@link FluentReflection#invoke(String)} メソッドから取得した値が <code>0</code> であること
+         * </pre>
+         * 
+         * <pre>
+         * ❏ 留意点
+         * なし
+         * </pre>
+         */
+        @Test
+        void testReturnIntegerFalseWithArgument() {
+            final FluentReflection<Integer> reflection = new FluentReflection<>(ReflectionTestDataSet.class);
+            final int actualResult = reflection.add(boolean.class, false).invoke("returnIntegerWithArgument");
+
+            assertEquals(0, actualResult);
+        }
+
+        /**
+         * <pre>
+         * ❏ 概要
+         * {@link FluentReflection} クラスの {@link FluentReflection#invoke(String)} メソッドの返却値を確認する。
+         * テストデータセットは {@link ReflectionTestDataSet#returnIntegerWithArguments(int, String, boolean)} を使用する。
+         * </pre>
+         * 
+         * <pre>
+         * ❏ 観点
+         * ・{@link FluentReflection#invoke(String)} メソッドから取得した値が <code>0</code> であること
+         * </pre>
+         * 
+         * <pre>
+         * ❏ 留意点
+         * なし
+         * </pre>
+         */
+        @Test
+        void testReturnIntegerWithArguments() {
+            final FluentReflection<Integer> reflection = new FluentReflection<>(ReflectionTestDataSet.class);
+            reflection.add(int.class, 1).add(String.class, "test").add(boolean.class, false);
+            final int actualResult = reflection.invoke("returnIntegerWithArguments");
+
+            assertEquals(1, actualResult);
+        }
     }
 }
