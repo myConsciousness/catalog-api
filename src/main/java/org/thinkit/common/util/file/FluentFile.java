94c2524ea7807a541546af8882d3487dc09f9a97
Kato Shinya <yourdream28@gmail.com>
2020-07-18 19:15:13 +0900

    fixed

--- src/main/java/org/thinkit/common/util/file/FluentFile.java
+++ src/main/java/org/thinkit/common/util/file/FluentFile.java
@@ -12,14 +12,7 @@
 
 package org.thinkit.common.util.file;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.nio.charset.StandardCharsets;
-import java.util.logging.FileHandler;
 
 import lombok.EqualsAndHashCode;
 import lombok.NonNull;
@@ -27,9 +20,7 @@ import lombok.Setter;
 import lombok.ToString;
 
 /**
- * ファイル操作に関する汎用的な処理を定義したクラスです。 {@link FileHandler} では以下の操作を提供しています。
- * <p>
- * {@link #write(String, String, String)}
+ * ファイル操作に関する汎用的な処理を定義したクラスです。
  *
  * @author Kato Shinya
  * @since 1.0
@@ -78,45 +69,6 @@ public final class FluentFile {
         if (!outputDirectory.exists()) {
             outputDirectory.mkdirs();
         }
-    }
-
-    /**
-     * ファイルへの書き込み処理を行います。<br>
-     * 出力先は {@link FluentFile} のインスタンス生成時に渡した文字列に<br>
-     * {@link #write(String, String, String)} の第１引数として渡した文字列を結合したものになります。<br>
-     * 出力先を変更した場合は {@link #setOutput(String)} を使用してください。 <br>
-     * <br>
-     * 書き込み処理で {@link IOException} が発生した場合は {@code false} を返却します。
-     *
-     * @param fileName  ファイル名
-     * @param extension 拡張子
-     * @param content   書き込む内容
-     * @return 書き込み処理が正常終了した場合は {@code true}、{@link IOException} が発生した場合は
-     *         {@code false}
-     *
-     * @exception IOException          出力処理が異常終了した場合
-     * @exception NullPointerException 引数として {@code null} が渡された場合
-     */
-    public boolean write(@NonNull String fileName, @NonNull String extension, @NonNull String content) {
-
-        final StringBuilder fullFileNameBuilder = new StringBuilder();
-        fullFileNameBuilder.append(fileName).append(extension);
-        final String fullFileName = fullFileNameBuilder.toString();
-
-        final StringBuilder filePathBuilder = new StringBuilder();
-        filePathBuilder.append(output).append("\\").append(fullFileName);
-
-        final File file = new File(new File(filePathBuilder.toString()).getParentFile(), fullFileName);
-
-        try (PrintWriter writer = new PrintWriter(
-                new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), StandardCharsets.UTF_8)))) {
-            writer.write(content);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return false;
-        }
-
-        return true;
     }
 
     /**
