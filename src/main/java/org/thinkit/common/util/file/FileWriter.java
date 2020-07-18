b4f5c6656581406b1fd0d89046b5177dba18be8c
Kato Shinya <yourdream28@gmail.com>
2020-07-19 00:00:54 +0900

    add methods for changing charset

--- src/main/java/org/thinkit/common/util/file/FileWriter.java
+++ src/main/java/org/thinkit/common/util/file/FileWriter.java
@@ -18,6 +18,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
 import lombok.AccessLevel;
@@ -28,6 +29,16 @@ import lombok.ToString;
 
 /**
  * ファイルへの書き込み処理に関する汎用的な処理を定義したクラスです。
+ *
+ * <pre>
+ * 使用例:
+ * <code>FileWriter.of(output).write(fileName, extension, content);</code>
+ * </pre>
+ *
+ * <pre>
+ * 書き込み時の文字コードを変更した使用例:
+ * <code>FileWriter.of(output).toCharsetUsAscii().write(fileName, extension, content);</code>
+ * </pre>
  *
  * @author Kato Shinya
  * @since 1.0
@@ -44,6 +55,12 @@ public final class FileWriter {
      */
     @Getter(AccessLevel.PRIVATE)
     private String output = "";
+
+    /**
+     * ファイル書き込み時の文字コード
+     */
+    @Getter(AccessLevel.PRIVATE)
+    private Charset charset = StandardCharsets.UTF_8;
 
     /**
      * デフォルトコンストラクタ
@@ -82,12 +99,25 @@ public final class FileWriter {
     }
 
     /**
-     * ファイルへの書き込み処理を行います。<br>
-     * 出力先は {@link FileWriter} のインスタンス生成時に渡した文字列に<br>
-     * {@link #write(String, String, String)} の第1引数として渡した文字列を結合したものになります。<br>
-     * 出力先を変更した場合は {@link #setOutput(String)} を使用してください。 <br>
-     * <br>
+     * ファイルへの書き込み処理を行います。 出力先は {@link FileWriter} のインスタンス生成時に渡した文字列に
+     * {@link #write(String, String, String)} の第1引数として渡した文字列を結合したものになります。
+     * <p>
+     * 書き込み時のデフォルト文字コードは {@code StandardCharsets.UTF_8} です。 書き込み時の文字コードを変更したい場合は
+     * {@link #write(String, String, String)} メソッドを呼び出す前に 文字コードを変更する
+     * {@link #toCharsetIso()} や {@link #toCharsetUsAscii()} を呼び出してください。
+     * 文字コードを変更するメソッドに関しては以下の参照を確認してください。
+     * <p>
      * 書き込み処理で {@link IOException} が発生した場合は {@code false} を返却します。
+     *
+     * <pre>
+     * 使用例:
+     * <code>FileWriter.of(output).write(fileName, extension, content);</code>
+     * </pre>
+     *
+     * <pre>
+     * 文字コードを変更した使用例:
+     * <code>FileWriter.of(output).toCharsetUsAscii().write(fileName, extension, content);</code>
+     * </pre>
      *
      * @param fileName  ファイル名
      * @param extension 拡張子
@@ -97,6 +127,13 @@ public final class FileWriter {
      *
      * @exception IOException          出力処理が異常終了した場合
      * @exception NullPointerException 引数として {@code null} が渡された場合
+     *
+     * @see #toCharsetIso()
+     * @see #toCharsetUsAscii()
+     * @see #toCharsetUtf16()
+     * @see #toCharsetUtf16Be()
+     * @see #toCharsetUtf16Le()
+     * @see #toCharsetUtf8()
      */
     public boolean write(@NonNull String fileName, @NonNull String extension, @NonNull String content) {
 
@@ -110,7 +147,7 @@ public final class FileWriter {
         final File file = new File(new File(filePathBuilder.toString()).getParentFile(), fullFileName);
 
         try (PrintWriter writer = new PrintWriter(
-                new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), StandardCharsets.UTF_8)))) {
+                new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), this.getCharset())))) {
             writer.write(content);
         } catch (IOException e) {
             e.printStackTrace();
@@ -118,5 +155,71 @@ public final class FileWriter {
         }
 
         return true;
+    }
+
+    /**
+     * 書き込み処理時の文字コードを {@code StandardCharsets.US_ASCII} に変更します。
+     * このメソッドは自分自身のインスタンスを返却するためメソッドチェーンで後続処理を行うことができます。
+     *
+     * @return 自分自身のインスタンス
+     */
+    public FileWriter toCharsetUsAscii() {
+        this.charset = StandardCharsets.US_ASCII;
+        return this;
+    }
+
+    /**
+     * 書き込み処理時の文字コードを {@code StandardCharsets.ISO_8859_1} に変更します。
+     * このメソッドは自分自身のインスタンスを返却するためメソッドチェーンで後続処理を行うことができます。
+     *
+     * @return 自分自身のインスタンス
+     */
+    public FileWriter toCharsetIso() {
+        this.charset = StandardCharsets.ISO_8859_1;
+        return this;
+    }
+
+    /**
+     * 書き込み処理時の文字コードを {@code StandardCharsets.UTF_8} に変更します。
+     * このメソッドは自分自身のインスタンスを返却するためメソッドチェーンで後続処理を行うことができます。
+     *
+     * @return 自分自身のインスタンス
+     */
+    public FileWriter toCharsetUtf8() {
+        this.charset = StandardCharsets.UTF_8;
+        return this;
+    }
+
+    /**
+     * 書き込み処理時の文字コードを {@code StandardCharsets.UTF_16BE} に変更します。
+     * このメソッドは自分自身のインスタンスを返却するためメソッドチェーンで後続処理を行うことができます。
+     *
+     * @return 自分自身のインスタンス
+     */
+    public FileWriter toCharsetUtf16Be() {
+        this.charset = StandardCharsets.UTF_16BE;
+        return this;
+    }
+
+    /**
+     * 書き込み処理時の文字コードを {@code StandardCharsets.UTF_16LE} に変更します。
+     * このメソッドは自分自身のインスタンスを返却するためメソッドチェーンで後続処理を行うことができます。
+     *
+     * @return 自分自身のインスタンス
+     */
+    public FileWriter toCharsetUtf16Le() {
+        this.charset = StandardCharsets.UTF_16LE;
+        return this;
+    }
+
+    /**
+     * 書き込み処理時の文字コードを {@code StandardCharsets.UTF_16} に変更します。
+     * このメソッドは自分自身のインスタンスを返却するためメソッドチェーンで後続処理を行うことができます。
+     *
+     * @return 自分自身のインスタンス
+     */
+    public FileWriter toCharsetUtf16() {
+        this.charset = StandardCharsets.UTF_16;
+        return this;
     }
 }
