0d144722a9340fd28ba61169e63ac62023f6b92c
Kato Shinya <yourdream28@gmail.com>
2020-07-26 00:36:41 +0900

    update document

--- src/main/java/org/thinkit/common/iterator/IterableNode.java
+++ src/main/java/org/thinkit/common/iterator/IterableNode.java
@@ -1,10 +1,44 @@
+/**
+ * Project Name : dev-utils<br>
+ * File Name : IterableNode.java<br>
+ * Encoding : UTF-8<br>
+ * Creation Date : 2020/07/25<br>
+ * <p>
+ * Copyright © 2020 Kato Shinya. All rights reserved.
+ * <p>
+ * This source code or any portion thereof must not be<br>
+ * reproduced or used in any manner whatsoever.
+ */
+
 package org.thinkit.common.iterator;
 
 import java.util.List;
 
+/**
+ * イテレート可能なノードの機能を抽象化したインターフェースです。
+ * <p>
+ * {@link FluentIterator} クラスの機能を使用する場合は必ず対象のクラスでこの {@link IterableNode}
+ * インターフェースを実装してください。
+ *
+ * @author Kato Shinya
+ * @since 1.0
+ * @version 1.0
+ *
+ * @see FluentIterator
+ */
 public interface IterableNode<T> {
 
+    /**
+     * イテレート対象ノード群のサイズを返却します。
+     *
+     * @return イテレート対象ノード群のサイズ
+     */
     public int size();
 
+    /**
+     * イテレート対象ノード群を返却します。
+     *
+     * @return イテレート対象ノード群
+     */
     public List<T> nodes();
 }
