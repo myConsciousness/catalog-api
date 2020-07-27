2d5295b8463890e732d95d727afd5368b5111710
Kato Shinya <yourdream28@gmail.com>
2020-07-27 11:36:44 +0900

    fixed package

--- src/test/java/org/thinkit/common/content/ContentLoaderTest.java
+++ src/test/java/org/thinkit/common/content/ContentLoaderTest.java
@@ -10,7 +10,7 @@
  * reproduced or used in any manner whatsoever.
  */
 
-package org.thinkit.common.util.content;
+package org.thinkit.common.content;
 
 import org.thinkit.common.key.ConditionNodeKey;
 import org.thinkit.common.key.Key;
