5b98be80c1d16c276e37ab65c00399a7dd62ac43
Kato Shinya <yourdream28@gmail.com>
2020-08-15 20:28:02 +0900

    add

---
+++ src/main/java/org/thinkit/common/catalog/Bracket.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2020 Kato Shinya.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.thinkit.common.catalog;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * ブラケットを管理するカタログです。
+ *
+ * @author Kato Shinya
+ * @since 1.0
+ * @version 1.0
+ */
+@RequiredArgsConstructor
+public enum Bracket implements Catalog<Bracket> {
+
+    /**
+     * 開始ブラケット
+     */
+    START(0, "["),
+
+    /**
+     * 終了ブラケット
+     */
+    END(1, "]");
+
+    /**
+     * コード値
+     */
+    @Getter
+    private final int code;
+
+    /**
+     * ブラケット
+     */
+    @Getter
+    private final String bracket;
+
+    /**
+     * {@link #START} 要素の文字列表現を返却します。
+     *
+     * @return {@link #START} 要素の文字列表現
+     *
+     * @see #START
+     */
+    public static String start() {
+        return START.getBracket();
+    }
+
+    /**
+     * {@link #END} 要素の文字列表現を返却します。
+     *
+     * @return {@link #END} 要素の文字列表現
+     *
+     * @see #END
+     */
+    public static String end() {
+        return END.getBracket();
+    }
+}
