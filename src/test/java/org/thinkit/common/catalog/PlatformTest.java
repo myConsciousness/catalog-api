d6271bfc99552d0a79dbc6a6359828aef45d67f1
Kato Shinya <yourdream28@gmail.com>
2020-08-17 18:16:43 +0900

    update

--- src/test/java/org/thinkit/common/catalog/PlatformTest.java
+++ src/test/java/org/thinkit/common/catalog/PlatformTest.java
@@ -1,13 +1,15 @@
-/**
- * Project Name : dev-utils<br>
- * File Name : PlatformTest.java<br>
- * Encoding : UTF-8<br>
- * Creation Date : 2020/06/26<br>
- * <p>
- * Copyright © 2020 Kato Shinya. All rights reserved.
- * <p>
- * This source code or any portion thereof must not be<br>
- * reproduced or used in any manner whatsoever.
+/*
+ * Copyright 2020 Kato Shinya.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package org.thinkit.common.catalog;
