c8eaf256f0f1748c06ca4bc2623153ba671a5c2e
Kato Shinya <kato.shinya.dev@gmail.com>
2021-06-06 00:40:44 +0900

    update document

--- src/main/java/org/thinkit/api/catalog/BiCatalog.java
+++ src/main/java/org/thinkit/api/catalog/BiCatalog.java
@@ -18,81 +18,111 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
- * An interface that defines common operations of the Catalog with tag.
+ * The interface that extends the functionality of Enum, a built-in feature of
+ * Java, and consolidates general-purpose functions used in many situations.
+ * This interface can be implements in existing Enum classes to easily add
+ * useful functionality. When implementing this interface, please specify the
+ * type of the class that implements this interface in the generic. The
+ * {@link BiCatalog} interface will then manage values of any type as tags, and
+ * the second generic type should be the type of value you want to manage in the
+ * Enum element.
+ *
+ * <p>
+ * This interface manages the code value used to identify each element of an
+ * Enum. The code value is an arbitrary numeric value that can be specified for
+ * each element of the Enum class that implements this interface; the code value
+ * for each element of the Enum does not need to be in the order of the
+ * elements, and can be specified in any way that the implementor of this
+ * interface chooses. However, since this code value is used to identify each
+ * element of the Enum, it is important that the specified value does not
+ * overlap with any other element in the Enum class.
+ *
+ * <p>
+ * The value managed as a tag can be any value as long as it conforms to the
+ * type specified in the generics. However, since Java does not currently allow
+ * primitive types to be specified in generics, only object types can be managed
+ * as tags. This specification may be improved in a later version of Java.
+ *
+ * <p>
+ * This interface provides generic functions based on the code values specified
+ * in the Enum class that implements this interface. For example, there is a
+ * {@link #hasCode(Class, int)} to determine whether the code value given as an
+ * argument is defined in the target Enum class, and there is a
+ * {@link #getEnum(Class, int)} that returns the Enum element associated with
+ * the number given as an argument from the target Enum class. Other generic
+ * methods are also provided, but all of them require the target Enum class to
+ * implement this interface.
+ *
+ * <p>
+ * The basic implementation and usage examples of this interface are briefly
+ * described below.
+ *
+ * <pre>
+ * <code>
+ * public enum EnumClass implements BiCatalog<EnumClass, String> {
+ *
+ *    ELEMENT_1(0, "test1"),
+ *
+ *    ELEMENT_2(1, "test2");
+ *
+ *    private int code;
+ *
+ *    private String tag;
+ *
+ *    EnumClass(int code) {
+ *        this.code = code;
+ *    }
+ *
+ *    &#64;Override
+ *    public int getCode() {
+ *        return this.code;
+ *    }
+ *
+ *    &#64;Override
+ *    public String getTag() {
+ *        return this.tag;
+ *    }
+ * }
+ * </code>
+ * </pre>
+ *
+ * <pre>
+ * <code>
+ * public class TestBiCatalog {
+ *
+ *     public static void main(String[] args) {
+ *         BiCatalog.hasCode(EnumClass.class, 1); // Returns true
+ *         BiCatalog.getEnum(EnumClass.class, 1); // Returns EnumClass#ELEMENT_2
+
+ *         BiCatalog.contains(EnumClass.class, "test1"); // Returns true
+ *         BiCatalog.getEnumByTag(EnumClass.class, "test1"); // Returns EnumClass#ELEMENT_1
+ *     }
+ * }
+ * </code>
+ * </pre>
  *
  * @author Kato Shinya
- * @since 1.0
- * @version 1.0
- *
- * @param <E> Catalog element
- * @param <T> Catalog tag type
+ * @since 1.0.0
  */
-public interface BiCatalog<E extends BiCatalog<E, T>, T> {
+public interface BiCatalog<E extends BiCatalog<E, T>, T> extends BiCatalogSupport<E, T> {
 
     /**
-     * Return a code value.
+     * Returns the {@link List} representation of this catalog class.
      *
-     * @return Code value
-     */
-    public int getCode();
-
-    /**
-     * Return a tag.
+     * <p>
+     * The {@link List} representation of this catalog class is sorted each element
+     * based on the code value specified for each element of the Enum class that
+     * implements the {@link Catalog} interface. This sorting process is based on
+     * the {@link Stream#sorted()} algorithm.
      *
-     * @return Tag
-     */
-    public T getTag();
-
-    /**
-     * Convert to an Enum class object.
-     *
-     * @return Converted an Enum class object
-     */
-    @SuppressWarnings("unchecked")
-    default E toEnum() {
-        return (E) this;
-    }
-
-    /**
-     * Check whether the code value of the Enum element is equal to the value passed
-     * as an argument.
-     *
-     * @param code Code value
-     * @return If the number passed as an argument is equal to the code value of the
-     *         Enum element, {@code true}, otherwise {@code false}
-     */
-    default boolean equalsByCode(int code) {
-        return getCode() == code;
-    }
-
-    /**
-     * Check whether the tag of the Enum element is equal to the tag passed as an
-     * argument.
-     *
-     * @param tag Tag
-     * @return If the number passed as an argument is equal to the code value of the
-     *         Enum element, {@code true} , otherwise {@code false}
-     */
-    default boolean equalsByTag(T tag) {
-        return Objects.equals(getTag(), tag);
-    }
-
-    /**
-     * Each element of the Enum class that implements this interface is sorted based
-     * on the code value defined for each element. The result of the sorting process
-     * is converted into a list structure with the order in which it is displayed
-     * and returned.
-     *
-     * @param <E>   Catalog element
-     * @param <T>   Catalog tag type
-     * @param clazz Enum class object to be operated on
-     * @return A list containing the values sorted based on the code values defined
-     *         for each element of the Enum class
+     * @param <E>   The type of Enum class
+     * @param <T>   The type of tag value
+     * @param clazz The Enum class to be sorted
+     * @return The sorted Enum elements based on the code value
      */
     public static <E extends BiCatalog<E, T>, T> List<E> getOrderedList(Class<? extends BiCatalog<E, T>> clazz) {
         return stream(clazz).sorted(Comparator.comparing(BiCatalog::getCode)).map(BiCatalog::toEnum)
@@ -100,91 +130,94 @@ public interface BiCatalog<E extends BiCatalog<E, T>, T> {
     }
 
     /**
-     * The code values defined for each element of the Enum class that implements
-     * the interface are compared with the numbers passed as arguments, and the Enum
-     * element that matches the values is returned.
+     * Returns the Enum element linked to the code value given as an argument from
+     * the target Enum class. If the target Enum class does not have an Enum element
+     * linked to the code value given as an argument, {@code null} is returned.
      *
-     * @param <E>   Catalog element
-     * @param <T>   Catalog tag type
-     * @param clazz Enum class object to be operated on
-     * @param code  Code value
-     * @return Enum element with a code value equal to the number specified as an
-     *         argument
+     * @param <E>   The type of Enum class
+     * @param <T>   The type of tag value
+     * @param clazz The target Enum class
+     * @param code  The code value linked to the Enum element
+     * @return The Enum element linked to the code value, or {@code null} if the
+     *         target Enum class does not have an Enum element linked to the code
+     *         value
      */
     public static <E extends BiCatalog<E, T>, T> E getEnum(Class<? extends BiCatalog<E, T>> clazz, int code) {
         return stream(clazz).filter(e -> e.equalsByCode(code)).map(BiCatalog::toEnum).findFirst().orElse(null);
     }
 
     /**
-     * The tag values defined for each element of the Enum class that implements the
-     * interface are compared with the specified value passed as arguments, and the
-     * Enum element that matches the values is returned.
+     * Returns the Enum element linked to the tag value given as an argument from
+     * the target Enum class. If the target Enum class does not have an Enum element
+     * linked to the tag value given as an argument, {@code null} is returned.
      *
-     * @param <E>   Catalog element
-     * @param <T>   Catalog tag type
-     * @param clazz Enum class object to be operated on
-     * @param tag   Tag value
-     * @return Enum element with a tag value equal to the specified value as an
-     *         argument
+     * @param <E>   The type of Enum class
+     * @param <T>   The type of tag value
+     * @param clazz The target Enum class
+     * @param tag   The tag value linked to the Enum element
+     * @return The Enum element linked to the tag value, or {@code null} if the
+     *         target Enum class does not have an Enum element linked to the tag
+     *         value
      */
     public static <E extends BiCatalog<E, T>, T> E getEnumByTag(Class<? extends BiCatalog<E, T>> clazz, T tag) {
         return stream(clazz).filter(e -> e.equalsByTag(tag)).map(BiCatalog::toEnum).findFirst().orElse(null);
     }
 
     /**
-     * The code value defined for each element of the Enum class that implements the
-     * interface is compared with the number specified as an argument. Return the
-     * Enum element whose value matches the code value as a key.
+     * Returns the {@link Map} representation of this catalog class.
      *
-     * @param <E>   Catalog element
-     * @param <T>   Catalog tag type
-     * @param clazz Enum class object to be operated on
-     * @return Map containing an Enum element with a code value equal to the number
-     *         specified as an argument as a code key
+     * <p>
+     * The structure of the map returned by this {@link #getMap} has the code value
+     * specified in the Enum element of the Enum class that implements this
+     * {@link Catalog} interface as the key and the Enum element linked to the code
+     * value as the value.
+     *
+     * @param <E>   The type of Enum class
+     * @param <T>   The type of tag value
+     * @param clazz The target Enum class
+     * @return The {@link Map} representation of this catalog class
      */
     public static <E extends BiCatalog<E, T>, T> Map<Integer, E> getMap(Class<? extends BiCatalog<E, T>> clazz) {
         return stream(clazz).collect(Collectors.toMap(BiCatalog::getCode, BiCatalog::toEnum));
     }
 
     /**
-     * This function determines whether the code value specified as an argument is
-     * defined in each element of the Enum class that implements the interface
-     * concerned.
+     * Checks if the target Enum class has an Enum element linked to the code value
+     * passed as an argument.
      *
-     * @param <E>   Catalog element
-     * @param <T>   Catalog tag type
-     * @param clazz Enum class object to be operated on
-     * @param code  Code value
-     * @return {@code true} if there is an Enum element with a code value equal to
-     *         the number specified as an argument, otherwise {@code false}
+     * @param <E>   The type of Enum class
+     * @param <T>   The type of tag value
+     * @param clazz The target Enum class
+     * @param code  The code value
+     * @return {@code true} if the target Enum class has an Enum element linked to
+     *         the code value passed as an argument, otherwise {@code false}
      */
     public static <E extends BiCatalog<E, T>, T> boolean hasCode(Class<? extends BiCatalog<E, T>> clazz, int code) {
         return stream(clazz).anyMatch(e -> e.equalsByCode(code));
     }
 
     /**
-     * This function determines whether the tag specified as an argument is defined
-     * in each element of the Enum class that implements the interface concerned.
+     * Checks if the target Enum class has an Enum element linked to the tag value
+     * passed as an argument.
      *
-     * @param <E>   Catalog element
-     * @param <T>   Catalog tag type
-     * @param clazz Enum class object to be operated on
-     * @param tag   Tag
-     * @return {@code true} if there is an Enum element with a code value equal to
-     *         the number specified as an argument, otherwise {@code false}
+     * @param <E>   The type of Enum class
+     * @param <T>   The type of tag value
+     * @param clazz The target Enum class
+     * @param tag   The tag value
+     * @return {@code true} if the target Enum class has an Enum element linked to
+     *         the tag value passed as an argument, otherwise {@code false}
      */
     public static <E extends BiCatalog<E, T>, T> boolean contains(Class<? extends BiCatalog<E, T>> clazz, T tag) {
         return stream(clazz).anyMatch(e -> e.equalsByTag(tag));
     }
 
     /**
-     * Converts the element group defined in the {@code clazz} argument to a
-     * {@link Stream} and returns it.
+     * Returns the {@link Stream} representation of the target Enum class.
      *
-     * @param <E>   Catalog element
-     * @param <T>   Catalog tag type
-     * @param clazz Enum class object to be operated on
-     * @return Streams of the Enum class
+     * @param <E>   The type of Enum class
+     * @param <T>   The type of tag value
+     * @param clazz The target Enum class
+     * @return The {@link Stream} representation of the target Enum class
      */
     public static <E extends BiCatalog<E, T>, T> Stream<? extends BiCatalog<E, T>> stream(
             Class<? extends BiCatalog<E, T>> clazz) {
